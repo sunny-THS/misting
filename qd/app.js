f63006412d5df9f0bddc70f3b915fe8208a74b5b
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-10-03 23:56:49 +0700

    sd

--- qd/app.js
+++ qd/app.js
@@ -1,47 +1,66 @@
 const API_URL = 'https://fir-9d177.firebaseio.com/ThongSo.json?&auh=YA2HldfmtXM9ZtNtAz5yetMTgxFpd8Gu2Dx8seta';
-const LOG_URL = 'https://script.google.com/macros/s/AKfycbwEMg3BHMVn7-_5cdoS6ExQTisCtotJUuXQv1_7kHiVlHMfqNc/exec?data=';
+const LOG_URL = 'https://script.google.com/macros/s/AKfycbwEMg3BHMVn7-_5cdoS6ExQTisCtotJUuXQv1_7kHiVlHMfqNc/exec';
 
 const temperature = document.querySelector('#_t');
 const humidity = document.querySelector('#_h');
 const pump = document.querySelector('.pump');
 
-var _t, _h, _p, ip;
+var ip;
 
 const database = firebase.database();
 
 app();
 
-function  app() {
+function app() {
   GetDB();
   setTimeout(getIPAddress, 500);
 }
 
-function logData(t, h, p) {
-  fetch(`${LOG_URL}{"temperature":${t},"humidity":${h},"Pump":${p}}`)
-    .catch();
-  console.log(`${LOG_URL}{"temperature":${t},"humidity":${h},"Pump": ${p}}`);
+function logData({
+  t,
+  h,
+  p
+}) {
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  fetch("https://script.google.com/macros/s/AKfycbwEMg3BHMVn7-_5cdoS6ExQTisCtotJUuXQv1_7kHiVlHMfqNc/exec", requestOptions)
+    .catch(handleErr);
 }
 
-function GetDB() {
-  fetch(API_URL)
-  .then(res => res.json())
-  .then(db => {
-    _t = db.NhietDo;
-    temperature.textContent = `${Math.round(db.NhietDo*100)/100} ᵒC`;
-
-    _h = db.DoAm;
-    humidity.textContent = `${db.DoAm} %`;
-
-    _p = db.PumpIsWork;
-    if(db.PumpIsWork == 1) {
-      pump.style.background = 'green';
-    }else {
-      pump.style.background = 'red';
-    }
-  })
-  .catch();
-  logData(_t, _h, _p);
+async function GetDB() {
+  let data = await (await fetch(API_URL).catch(handleErr)).json();
+  let thp_ = {
+    t: data.NhietDo,
+    h: data.DoAm,
+    p: data.PumpIsWork
+  };
+  await returnData(thp_);
+  await logData(thp_);
+  // console.log(thp_);
   setTimeout(GetDB, 500);
+}
+
+function returnData({
+  t,
+  h,
+  p
+}) {
+  temperature.textContent = `${Math.round(t*100)/100}ᵒC`;
+  humidity.textContent = `${h}%`;
+  pump.style.background = p == 1 ? 'green' : 'red';
+}
+
+function handleErr(err) {
+  let resp = new Response(
+    JSON.stringify({
+      code: 400,
+      message: 'Stupid network Error'
+    })
+  );
+  return resp;
 }
 
 function getIPAddress() {
@@ -49,7 +68,6 @@ function getIPAddress() {
     .then(res => res.json())
     .then(db => {
       let scores = db.IP;
-      ip = db.IP;
       let devices = Object.keys(scores);
       devices.forEach(device => {
         const div = document.querySelector('._IP');
@@ -70,21 +88,3 @@ function getIPAddress() {
       });
     });
 }
-
-// function SetData() {
-//   database.ref().set({
-//     DoAm: _h,
-//     NhietDo: _t,
-//     IP: {
-//       DHT: {
-//         name: ip['DHT'].name,
-//         val: ip['DHT'].val
-//       },
-//       Relay: {
-//         name: ip['Relay'].name,
-//         val: ip['Relay'].val
-//       }
-//     },
-//     PumpIsWork: _p,
-//   });
-// }
